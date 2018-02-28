e90321521003cc0f85195d17eb6c3a86c2ed3b13
Robin.Ong <robin.ong@bigcommerce.com>
2018-03-01 09:43:49 +1100

    Import Checkout SDK

--- assets/js/theme/checkout/checkout.jsx
+++ assets/js/theme/checkout/checkout.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { createCheckoutService } from '@bigcommerce/checkout-sdk';
 
 export default class CheckoutComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.service = createCheckoutService();
+    }
+
+    componentDidMount() {
+        this.service.loadCheckout()
+            .then(({ checkout }) => console.log(checkout.getCart()));
+    }
+
     render() {
         return (
             <section>
