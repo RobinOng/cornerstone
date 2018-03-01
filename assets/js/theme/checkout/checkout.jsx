0dfbbbbeeca5e7566a96e81372f49ba51b4b0dc1
Robin.Ong <robin.ong@bigcommerce.com>
2018-03-01 11:18:04 +1100

    Add cart component

--- assets/js/theme/checkout/checkout.jsx
+++ assets/js/theme/checkout/checkout.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { createCheckoutService } from '@bigcommerce/checkout-sdk';
+import Snackbar from 'material-ui/Snackbar';
+import Cart from './cart';
 
 export default class CheckoutComponent extends React.Component {
     constructor(props) {
         super(props);
 
         this.service = createCheckoutService();
+        this.state = { isLoading: true };
     }
 
     componentDidMount() {
         this.service.loadCheckout()
-            .then(({ checkout }) => console.log(checkout.getCart()));
+            .then(() => this.setState({ isLoading: false }));
+
+        this.subscriber = this.service.subscribe((state) => {
+            this.setState(state);
+        });
     }
 
     render() {
+        if (this.state.isLoading) {
+            return this._renderLoadingState();
+        }
+
+        const { checkout } = this.service.getState();
+
         return (
             <section>
-                Checkout
+                <Cart cart={ checkout.getCart() } />
             </section>
+        );
+    }
+
+    _renderLoadingState() {
+        return (
+            <Snackbar
+                anchorOrigin={ { vertical: 'top', horizontal: 'center' } }
+                open={ true }
+                message="Loading..." />
         );
     }
 }
