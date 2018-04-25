31cdc99cf73fe90cccfb1b8790a41df7f243d361
Robin Ong <robin.ong@syd-mbp-ojha.local>
2018-04-25 13:44:16 +1000

    Import sdk example from separate repo

--- assets/js/theme/checkout.jsx
+++ assets/js/theme/checkout.jsx
@@ -1,5 +1,5 @@
 import PageManager from './page-manager';
-import Checkout from './checkout/checkout';
+import Checkout from '@bigcommerce/checkout-sdk-example';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
